432faead0ba5a530a7aba936ed787582a189b1e3
yysimple <5325809+yysimple@user.noreply.gitee.com>
2021-05-22 13:16:05 +0800

    [新增]：初始化项目

---
+++ src/main/java/com/simple/testjenkins/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.simple.testjenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 项目: test-jenkins
+ * <p>
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2021-05-22 13:04
+ **/
+@RestController
+public class HelloController {
+
+    @GetMapping("/sayHello")
+    public String sayHello() {
+        return "Hello jenkins";
+    }
+
+}
